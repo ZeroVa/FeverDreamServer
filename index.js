f8de3f55231dbdeafdbfdd9ba885b638c86a3770
ZeroVa <pie.is.forever@gmail.com>
2024-03-30 18:06:48 +0900

    Made hono work

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 import { Hono } from 'hono'
+import { serve } from '@hono/node-server'
 
 const app = new Hono()
 
 app.get('/generateBackground', (c) => {
 	// Hit stability for some image n shit
 	let imageUrl = "https://placehold.co/600x400"
-	c.json({
+	return c.json({
 		backgroundImageUrl: imageUrl	
 	})
+})
+
+serve({
+	fetch: app.fetch,
+	port: 8787,
 })
