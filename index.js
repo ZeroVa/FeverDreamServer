10ad50e914ab19a80c1ba12135d060dbdfbd1f2d
ZeroVa <pie.is.forever@gmail.com>
2024-03-30 22:29:13 +0900

    Made stability api work as GET url for phaser images. Fixed image loading for binaries

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app = new Hono()
 app.use('/*', cors())
 app.get('/generateBackground', async (c) => {
 	// Hit stability for some image n shit
-	let {data} = await axios.post('https://api.stability.ai/v2beta/stable-image/generate/core',{
+	let res = await axios.post('https://api.stability.ai/v2beta/stable-image/generate/core',{
 			'prompt': "Point and click adventure scene background image of an empty room in what looks like an art deco apartment building in new york. There is a view of the nyc skyline from one of the small windows in the room. We can see a locked door. in video game art style, the floor should meet the bottom of the back wall 2/3 up from the bottom of the image, 2d, beneath a steel sky style",
 			'aspect_ratio': '3:2',
 			'negative_prompt': 'realism, portrait, 3d, photo'
@@ -22,21 +22,23 @@ app.get('/generateBackground', async (c) => {
 			'accept': 'application/json'
 		}
 	})
-
-	let writtenImage = fs.writeFileSync(`./tmp/bg-${(new Date().getTime())}.png`, Buffer.from(data.image, 'base64'))
+	let data = res.data.image
+	let image = Buffer.from(data, 'base64')
+	let writtenImage = fs.promises.appendFile(`./tmp/bg-${(new Date().getTime())}.png`, image)
 
 	c.status(200)
 	c.header('Content-type', 'image/png')
-	return c.body(data.image)
+	c.header('Content-length', image.length)
+	return c.body(image)
 })
 
 app.get('/image/:id', async (c) => {
 	const imageId = c.req.param('id')
 
-	let image = fs.readFileSync('./bg.png', 'base64')
+	let image = fs.readFileSync('./bg.png')
 	c.status(200)
 	c.header('Content-Type', 'image/png')
-	return c.body(Buffer.from(image, 'base64'))
+	return c.body(Buffer.from(image))
 })
 
 serve({
